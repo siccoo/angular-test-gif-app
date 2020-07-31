44cdbbe7003af120f46b327fa5d3c554fe7afe60
siccoo <obichilaka@gmail.com>
2020-07-31 18:37:50 +0100

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers'
 import './index.css';
 import App from './App';
+import Header from './components/header';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Header />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
